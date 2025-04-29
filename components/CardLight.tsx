7402a96f4adb41f485979c7da40fda4a47ad8a29
Phassakor <best20865@gmail.com>
2025-04-29 18:01:40 +0700

    fix

---
+++ components/CardLight.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import "../app/cardlight.css";
+
+export default function CardLight() {
+  return (
+    <section
+      className={`card`}
+    >
+     <div className="lines">
+
+     </div>
+      
+    </section>
+  );
+}
