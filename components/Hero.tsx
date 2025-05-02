b621ecea3d132eb552261e39abb23aa15f1284b9
Phassakor <best20865@gmail.com>
2025-05-02 16:02:10 +0700

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,9 +13,9 @@ export default function Hero() {
       id="home"
       className={`min-h-screen flex items-center justify-center px-4 sm:px-8 ${theme === "dark" ? "DarkMode" : "LightMode"}`}
     >
-      <div className={`max-w-3xl text-center p-5 rounded-lg transition-all duration-500 ease-in-out h-[250px] hover:h-[400px] z-10 ${theme === "dark" ? "shadow-lg" : "shadow-lg"}`}>
+      <div className={`card max-w-3xl text-center p-10 rounded-lg transition-all duration-500 ease-in-out h-[240px] hover:h-[400px] z-10 ${theme === "dark" ? "shadow-lg" : "shadow-lg"}`}>
         <motion.h1
-          className="text-4xl sm:text-5xl font-extrabold mb-4"
+          className="sm:text-2xl md:text-4xl xl:text-5xl text-xl font-extrabold mb-4"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
@@ -24,12 +24,12 @@ export default function Hero() {
         </motion.h1>
 
         <motion.h2
-          className="text-xl sm:text-2xl font-medium mb-6"
+          className="md:text-xl xl:text-2xl text-[16px] font-medium mb-6"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2, duration: 0.6 }}
         >
-          Frontend Developer | React / Next.js Enthusiast
+          Frontend Developer | coding Enthusiast
         </motion.h2>
 
         <motion.p
@@ -38,10 +38,25 @@ export default function Hero() {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.4, duration: 0.6 }}
         >
-          ผมรักการสร้างเว็บไซต์ที่เร็ว สวย และใช้งานง่าย ด้วยประสบการณ์ใน Next.js, Tailwind และ UX ที่ดี
+          ผมรักการสร้างเว็บไซต์ที่เร็ว สวย และใช้งานง่าย ด้วยประสบการณ์ใน Framework ที่ใช้ และ UX ที่ดี <br />
+          กำลังพัฒนาทักษะอย่างต่อเนื่องเพื่อก้าวไปสู่การเป็น Full Stack Developer ในอนาคต
         </motion.p>
 
+        <motion.div
+          className="flex justify-center gap-4 mt-20"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6, duration: 0.6 }}
+        >
+          <p className="bgHeroGIF text-[#F9FAFB] py-4 px-8 rounded-xl shadow-xl cursor-pointer">Dowload Resume</p>
+        </motion.div>
+
+        <span className="top"></span>
+        <span className="right"></span>
+        <span className="bottom"></span>
+        <span className="left"></span>
       </div>
+
 
       <div className="lines z-0">
         <div className="line"></div>
